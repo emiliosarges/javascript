59001398bb957928d83a7728181d02e0092a1e64
Emílio Silva <emiliosarges@gmail.com>
2025-01-04 15:36:39 -0300

    Atualização de pastas

---
+++ repetições/ambiente.js
@@ -0,0 +1,19 @@
+/*let c = 1
+
+while (c <= 6) {
+    console.log (`Passo ${c}`)
+    c++
+} 
+
+
+let c =1 
+do {
+    console.log (`Passo ${c}`)
+    c++
+} while (c <= 6) */
+
+console.log('Vai começar... ')
+for (let c = 1; c <= 4; c++) {
+    console.log (`Passo ${c}`)
+}
+console.log('FIM')
