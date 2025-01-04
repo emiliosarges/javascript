cff173875a90ebd7f3e9a62c8a08bda72b87eaff
Emílio Silva <emiliosarges@gmail.com>
2025-01-04 10:10:35 -0300

    finalização exercício hora do dia

--- ex014/script.js
+++ ex014/script.js
@@ -1,17 +1,21 @@
 function carregar() {
-    let msg = window.document.getElementById('msg')
-    let img = window.document.getElementById('imagem')
-    let data = new Date()
-    let hora = data.getHours()
+    let msg = window.document.getElementById('msg');
+    let img = window.document.getElementById('imagem');
+    let data = new Date();
+    let hora = data.getHours();
    
-    msg.innerHTML = `Agora são ${hora} horas.`
+    msg.innerHTML = `Agora são ${hora} horas.`;
 
     if (hora >= 0 && hora < 12) {
         img.src = 'manha.jpg';
+        document.body.style.background = '#e2cd9f';
+
     } else if (hora >= 12 && hora < 19) {
         img.src = 'tarde.jpg';
+        document.body.style.background = '#b9846f';
     } else {
         img.src = 'noite.jpg';
+        document.body.style.background = '#515154';
     }  
 }
 
